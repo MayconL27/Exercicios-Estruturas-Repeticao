33e98ec77f2f639276064d8d05621ccc4672835f
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-27 16:53:29 -0300

    MaiorEMedia incompleto

---
+++ src/MaiorEMedia.java
@@ -0,0 +1,13 @@
+public class MaiorEMedia {
+    public static void main(String[] args) {
+        /** Faça um programa que leia 5 números e informe o maior número 
+         * e a média desses números. 
+         */
+
+         Scanner sc = new Scanner(System.in);
+
+         
+
+         
+    }
+}
