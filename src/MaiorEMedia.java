4756d0e67b9f077a31f6eed10c1687226e01197d
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-31 15:39:25 -0300

    Maior e Media

--- src/MaiorEMedia.java
+++ src/MaiorEMedia.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class MaiorEMedia {
     public static void main(String[] args) {
         /** Faça um programa que leia 5 números e informe o maior número 
          * e a média desses números. 
          */
-
          Scanner sc = new Scanner(System.in);
 
-         
+         int num;
+         int contador = 0;
+         int media = 0;
+         int maior = 0;
 
+         do {
+             System.out.println("Digite um número");
+             num = sc.nextInt();  
+             
+            if (num > maior) {
+                maior =num;
+                System.out.println(num + " é maior que " + maior);
+            } else {
+                System.out.println(maior + " é maior que " + num);
+            }
+            media = media + num;
+            contador++;
+            
+         } while (contador < 5);
+            media = media/5;
+            System.out.println("A média dos 5 números é: " + media);
          
     }
 }
