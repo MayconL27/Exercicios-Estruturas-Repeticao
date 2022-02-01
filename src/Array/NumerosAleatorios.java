a7daa05c5aeab03ba19bdc36593f8d9d34a2e4f8
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-01 15:27:23 -0300

    Numeros Aleatorios Exercicio

---
+++ src/Array/NumerosAleatorios.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        /** Faça um Programa que leia 20 números 
+         * inteiros aleatórios (entre 0 e 100) 
+         * armazene-os num vetor.Ao final mostre 
+         * os números e seus sucessores. 
+         */
+        
+    }
+}
